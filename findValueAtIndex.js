7005ec2084cc2781a9bd1b00d7789662412322dd
hurairaFurqan <hurairafurqan13@gmail.com>
2024-03-02 05:05:22 +1100

    questions

--- findValueAtIndex.js
+++ findValueAtIndex.js
@@ -14,7 +14,41 @@ a.next = b;
 b.next = c;
 c.next = d;
 
-
+/* My approach to iterative Method
 const findValueAtIndex = (head, index) => {
-    
-}
+    let current = head;
+    let count = 0
+    while (count <= index) {
+        if (current === null) {
+            console.log("Index not found");
+            break
+        }
+        if (count === index) {
+            console.log(current.val);
+            break
+        } else {
+            current = current.next
+            count++
+        }
+    }
+} */
+
+/* Optimize approach to iterative Method
+const findValueAtIndex = (head, index) => {
+    let current = head;
+    let count = 0
+    while(current !== null){
+        if (count === index) return current.val;
+        current = current.next;
+        count++;
+    }
+    return null;
+} */
+
+/* Recursive Approach
+const findValueAtIndex = (head, index) => {
+    if (head === null) return null;
+    if (index === 0) return head.val;
+    return findValueAtIndex(head.next, index - 1)
+} */
+console.log(findValueAtIndex(a, 2));
